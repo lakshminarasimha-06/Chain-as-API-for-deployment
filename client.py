111840f6628b17c3d287b150fd3e630c7271faf6
Lucky <114739266+lakshminarasimha-06@users.noreply.github.com>
2024-08-07 20:31:33 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,29 @@
+import requests
+import streamlit as st
+
+def get_ollama_response(input_text):
+    response=requests.post(
+    "http://localhost:8000/poem/invoke",
+    json={'input':{'topic':input_text}})
+    return response.json()['output']
+
+st.title('Langchain Demo With phi3 API using Langserve')
+input_text=st.text_input("Write a poem on")
+
+if input_text:
+    st.write(get_ollama_response(input_text))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
